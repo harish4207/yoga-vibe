5f609e2225319b2c52d908d5dafee8301c71f93f
harish <you@example.com>
2025-06-15 22:40:53 +0530

    fix: refine AuthRequest user type with mongoose.Document

--- server/src/types/express.d.ts
+++ server/src/types/express.d.ts
@@ -1,8 +1,9 @@
 import { Request } from 'express';
+import { Document } from 'mongoose';
 import { IUser } from '../models/User';
 
 export interface AuthRequest extends Request {
-  user?: IUser;
+  user?: IUser & Document;
 }
 
 // This is a placeholder to ensure this file is treated as a module
